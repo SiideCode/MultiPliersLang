d020e708fda9f63a3591773b4725a2102a19434f
SiideCode <maksimka.repko@mail.ru>
2025-05-22 22:23:12 +0300

    i forgotr

--- src/LexerException.hx
+++ src/LexerException.hx
@@ -1,10 +1,10 @@
-import ScanningLexer.TokenT;
+import Lexer.TokenT;
 import haxe.Exception;
 
 class LexerException extends Exception {
 	public var token:TokenT;
 
-	public function new(message:String, token:ScanningLexer.TokenT, ?previous:Null<Exception>, ?native:Null<Any>) {
+	public function new(message:String, token:TokenT, ?previous:Null<Exception>, ?native:Null<Any>) {
 		super(message, previous, native);
 		this.token = token;
 	}
